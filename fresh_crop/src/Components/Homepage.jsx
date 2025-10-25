008210b89b3c8b610d3771ab73eab96b3ceb8c36
benxta4424 <podean.beni@gmail.com>
2025-10-26 00:16:42 +0300

    adding the pictures neccessary for each product and styling them

---
+++ fresh_crop/src/Components/Homepage.jsx
@@ -0,0 +1,42 @@
+import Box from './ProductBox'
+import marar from './imgs/anna-evans-h7sIa4E5mIg-unsplash.jpg'
+import varza from './imgs/anna-evans-SztWGpNJQEk-unsplash.jpg'
+import ceapa from './imgs/christopher-previte-7CcXR5wIhEY-unsplash.jpg'
+import vinete from './imgs/christy-hinko-nbbwiNujmxM-unsplash.jpg'
+import cartofi from './imgs/engin-akyurt-O9p8KIv39gA-unsplash.jpg'
+import rosii from './imgs/justus-menke-zRqRhIJqdnI-unsplash.jpg'
+import ardeiRosu from './imgs/theo-crazzolara-gfmqaW_Pk20-unsplash.jpg'
+
+export default function Homepage() {
+    const productList = [
+        {name: "Marar", price: 15, path: marar},
+        {name: "Varza", price: 3, path: varza},
+        {name: "Ceapa", price: 10, path: ceapa},
+        {name: "Vinete", price: 8, path: vinete},
+        {name: "Cartofi", price: 2, path: cartofi},
+        {name: "Rosii", price: 20, path: rosii},
+        {name: "Ardei Rosu", price: 10, path: ardeiRosu}
+    ]
+
+
+    return (
+        <>
+            <div className="homepageContainer">
+                <div className="homepageButtons"></div>
+                
+                <div className="productsContainer">
+                    { productList.map((product, index) => (
+                        <Box 
+                        key = {index}
+                        searchImage = {product.path}
+                        imageName = {product.name}
+                        productPrice = {product.price}
+                        />
+                    ))}
+                </div>
+
+                <div className="homepageBottomContainer"></div>
+            </div>
+        </>
+    )
+}
--- fresh_crop/src/Components/ProductBox.jsx
+++ fresh_crop/src/Components/ProductBox.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-
-export default function Box(prop_key, imageName) {
+export default function Box(props) {
     return (
         <>
-            <div className="box_container">
                 {/* scriem cumva titlul si pretul produsului peste poza */}
                 <div className="item_picture">
-                    <img src="src\Components\img\anna-evans-h7sIa4E5mIg-unsplash.jpg" alt="marar" key={prop_key} className={imageName} />
-                    <div className="item_title">pic title</div>
-                    <div className="item_price">pic price</div>
+                    <img src={props.searchImage} alt="marar" className="boxImages" />
+                    <div className="item_title">{props.imageName}</div>
+                    <button className="buyingButton">{props.productPrice} RON</button>
                 </div>
-            </div>
+
         
         </>
     )
