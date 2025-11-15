b796335e5e0669d236d287a530ed21f6ef557edb
benxta4424 <podean.beni@gmail.com>
2025-11-15 03:21:55 +0200

    creating the about page with a clip in the background to make it look nice

--- fresh_crop/src/Components/About.jsx
+++ fresh_crop/src/Components/About.jsx
@@ -6,9 +6,14 @@ export default function About() {
         <>
             <div className="aboutContainer">
                 <Link to={"/"} className="goBack">Back</Link>
-                
+                <div className="aboutUs">
+                    <div className="titluAbout">About US</div>
+                    <div className="descriereAbout">Suntem PODPIC și noi suntem fermierii! Nu există intermediari. Primești legume proaspete direct din grădina noastră, de la pământ la masa ta. Mai proaspăt și mai cinstit de atât nu se poate.</div>
+                    <div className="linkuriAbout"></div>
+                </div>
+
                 <div className="clipContainer" >
-                    <video src="src\Components\imgs\clip_legume.mp4" autoPlay loop muted className="clip" width="40%" height="30%"></video>
+                    <video src="src\Components\imgs\clip_legume.mp4" autoPlay loop muted className="clip" height={150} width={240}></video>
                 </div>
 
             </div>
