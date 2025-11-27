0cc7ab261cd8af12dc629dab8b47c21c1cc9d016
benxta4424 <podean.beni@gmail.com>
2025-11-27 13:01:30 +0200

    hardcoding the label for now to see if it looks good or not

--- fresh_crop/src/Components/AboutPLant.jsx
+++ fresh_crop/src/Components/AboutPLant.jsx
@@ -1,16 +1,19 @@
+import spanac from "./imgs/rachel-clark-rehLLLNNP94-unsplash.jpg"
+
 export default function AboutPlant(props) {
     return (
-        <>
-            <div className="plantPhoto">{props.photo}</div>
+        // Added the wrapper div with the class
+        <div className="aboutPlantContainer"> 
+            <div className="plantPhoto"><img src={spanac} alt="" /></div>
             <div className="plantInfo">
-                <div className="plantDescription"></div>
+                <div className="plantDescription">Short description</div>
                 <div className="plantStats">
-                    <div className="calories"></div>
-                    <div className="fiber"></div>
-                    <div className="fats"></div>
-                    <div className="sodium"></div>
+                    <div className="calories">Calories: 10g</div>
+                    <div className="fiber">Fibers: 15g</div>
+                    <div className="fats">Fats: 4g</div>
+                    <div className="sodium">Sodium: 0g</div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
